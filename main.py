fc928b5bb40e2c79a40d09006fca78c526a1a872
coderpower0 <coderpower0@gmail.com>
2019-06-18 14:59:26 +0800

    add function

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-# TODO: add algorithm that calculate percent rate
-# TODO: add algorithm that calculate interest rate
-# TODO: add algorithm that calculate monthly payment
+loan = float(input("sila masukkan jumlah pinjaman yang diambil: "))
+interest_rate = float(input("sila masukan kadar faedah: "))
+year = float(int(input("sila masukkan tempoh bayaran balik dalam tahun: ")))
+
+
+def monthly_payment(loan, interest_rate, year):
+    month = year * 12
+    interest_rate = interest_rate / 100
+    interest = interest_rate * loan * year
+    loan = interest + loan
+    monthly_payment = loan / month
+
+    return monthly_payment
+
 
