601eb50a46273578765e0cbdaae863491db1268d
coderpower0 <coderpower0@gmail.com>
2019-06-24 22:49:55 +0800

    making my repo more organized

--- penyelesaian.py
+++
@@ -1,8 +0,0 @@
-# snippet kod di bawah akan menentukan samada berat ikan gred a lebih dari 1 atau tidak
-# jika berat ikan a lebih kurang dari 1 jumlah ikan a akan tetap sama dan tidak mendapat
-# diskaun jika tidak jumlah ikan a akan diberi diskaun sebanyak 15%
-
-if berat_ikan_a < 1:
-    jumlah_ikan_a = jumlah_ikan_a
-else:
-    jumlah_ikan_a * 15 /100
