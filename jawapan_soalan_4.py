723369548f963bc8fea7c41de1a9df0043b088de
coderpower0 <coderpower0@gmail.com>
2019-06-24 22:45:52 +0800

    change file name

--- jawapan_soalan_4.py
+++ jawapan_soalan_4.py
@@ -1,12 +1,16 @@
 jumlah_bayaran = float(input("sila masukkan jumlah bayaran: "))
 
 if jumlah_bayaran in range(100, 301):
+
     jumlah_bayaran = jumlah_bayaran * 10 / 100
     print(jumlah_bayaran)
+
 elif jumlah_bayaran > 300:
-    jumlah_bayaran = jumlah_bayaran *20/100
+
+    jumlah_bayaran = jumlah_bayaran * 20 / 100
     print(jumlah_bayaran)
     print("anda layak menerima sampul hari raya")
-else:
-    print(jumlah_bayaran)
 
+else:
+
+    print(jumlah_bayaran)
